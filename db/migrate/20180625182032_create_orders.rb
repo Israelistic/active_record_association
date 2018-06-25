473bbedc5c489ea18721dbfa93fc7e80b149bed4
Israelistic <haguyle@gmail.com>
2018-06-25 15:51:47 -0400

    added model relationship, and db:seed the tables

--- db/migrate/20180625182032_create_orders.rb
+++ db/migrate/20180625182032_create_orders.rb
@@ -1,9 +1,9 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-      t.inetger :order_number
+      t.integer :order_number
       t.integer :date
-      t.ineger :customer_id
+      t.integer :customer_id
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# haggai = Customer.create!(email: "haggai@email.com", mailing_address: "bitmaker, king st", name: "haggai")
+daniel = Customer.new(email: "daniel@email.com", mailing_address: "bitmaker, king st", name: "daniel")
+natalie =  Customer.new(email: "natalie@email.com", mailing_address: "bitmaker, king st", name: "natalie")
+
+# Order.create(order_number: 2001, date: 2018-06-25, customer: haggai)
+Order.create(order_number: 2001, date: 2018-06-25, customer: daniel)
+Order.create(order_number: 2001, date: 2018-06-25, customer: natalie)
